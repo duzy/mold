1628220656b7cc84b857736d00baa99e3d293416
Duzy Chan <code@extbit.io>
2019-09-15 11:00:21 +0800

    Change email

--- include/mold/vm/details/context_cursors.hpp
+++ include/mold/vm/details/context_cursors.hpp
@@ -1,7 +1,7 @@
 /**
  *  \file context_cursors.hpp
  *
- *  Copyright 2016 Duzy Chan <code@duzy.info>
+ *  Copyright 2016 Duzy Chan <code@extbit.io>
  *  
  *  Distributed under the Boost Software License, Version 1.0. (See accompanying
  *  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
